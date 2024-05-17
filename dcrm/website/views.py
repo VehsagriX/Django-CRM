02e8634c48a66ba8b0f57a68b1ecdc183758759f
Xasan Vehsagri <Khasan.irgashev@gmail.com>
2024-05-17 15:35:47 +0500

    Final project

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import  SingUpForm, AddRecordForm
+from .models import Record
 def home(request):
+    records = Record.objects.all()
+
+
     # Cheak to see if loggin in
     if request.method == 'POST':
         username = request.POST['username']
@@ -16,7 +21,7 @@ def home(request):
             messages.success(request, 'Ошибка, попробуй снова...')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -25,4 +30,67 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SingUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Успешная регистрация')
+            return redirect('home')
+    else:
+        form = SingUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'Вам необходимо войти в систему, чтобы просмотреть эту страницу...')
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Успешно удалено')
+        return redirect('home')
+    else:
+        messages.success(request, 'Вам необходимо войти в систему, чтобы просмотреть эту страницу...')
+        return redirect('home')
+
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Запись добавлена')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, 'Вы должны быть зарегистрированы')
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Запись обновлена')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, 'Вы должны быть зарегистрированы')
+        return redirect('home')
+
